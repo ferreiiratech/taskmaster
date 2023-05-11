4fb7e99944cd238aa2d1ccdfa4d8e3322ef96429
Leonardo Ferreira <leonardoferreira172001@gmail.com>
2023-05-11 01:54:41 -0300

    created validate user

--- src/controllers/TaskController.js
+++ src/controllers/TaskController.js
@@ -1,10 +1,15 @@
+const model = require("../models/modelConfig");
+
 const rootControll = (req, res) => {
   return res.render("index");
 };
 
-const registerUser = (req, res) => {
-  res.send("Cadastro realizado com sucesso");
-  console.log(req.file.path);
+const registerUser = async (req, res) => {
+  const resposta = await model.validateUser(req, res);
+
+  res.send(resposta);
+
+  //   console.log(req.file.path);
 };
 
 module.exports = {
--- src/models/modelConfig.js
+++ src/models/modelConfig.js
@@ -1,9 +1,53 @@
-const database = require('../database/db')
+const database = require("../database/db");
+const COLUMNS = ["name", "email", "password", "img_profile"];
 
-const registerUserDatabase = () => {
+const registerUserDatabase = (values) => {
+  const INSERT_QUERY = `INSERT INTO users (${COLUMNS.join(", ")}) VALUES (${values})`;
+  database.query(INSERT_QUERY, (error, results, fields) => {
+    if (error) throw error;
+  });
+};
 
-}
+const verifyUser = async (email) => {
+  const SELECT_QUERY = `SELECT * FROM users WHERE email = '${email}'`;
+  try {
+    const results = await database.query(SELECT_QUERY);
+    return results[0];
+  } catch (error) {
+    throw error;
+  }
+};
 
-module.exports  = {
-    registerUserDatabase
-}
+const validateUser = async (req, res) => {
+  try {
+    const {
+      "input-name-cad": name,
+      "input-email-cad": email,
+      "input-pass-cad": password,
+    } = req.body;
+
+    const img_profile = req.file.path.replace(/\\/g, "\\\\");
+
+    const emailExists = await verifyUser(email);
+
+    if (emailExists[0]) {
+      return "Email está em uso. Escolha outro";
+    } else {
+      const values = [
+        `'${name}'`,
+        `'${email}'`,
+        `'${password}'`,
+        `'${img_profile}'`,
+      ];
+      registerUserDatabase(values);
+      return "Cadastro realizado com sucesso";
+    }
+  } catch (error) {
+    console.log(error);
+    res.send("Ocorreu um erro ao realizar o cadastro");
+  }
+};
+
+module.exports = {
+  validateUser,
+};
